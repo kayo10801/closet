3c3a9f8eec35b951571fbc42aeceeb850d141f46
kayo10801 <98790416+kayo10801@users.noreply.github.com>
2022-03-23 01:17:18 +0900

    フロント

--- app/models/clothe.rb
+++ app/models/clothe.rb
@@ -8,7 +8,8 @@ class Clothe < ApplicationRecord
   def self.search(search)
     
     if search != ""
-      Clothe.where(['season LIKE(?) OR color LIKE(?)', "%#{search}%","%#{search}%"])
+      Clothe.where(['season LIKE(?) OR color LIKE(?) OR memo LIKE(?) OR date LIKE(?)', 
+        "%#{search}%","%#{search}%","%#{search}%","%#{search}%"])
     else
       Clothe.all
     end
