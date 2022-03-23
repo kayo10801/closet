1ce0a2265a85f95921cbaefd33bffcd2c0fa6dca
kayo10801 <98790416+kayo10801@users.noreply.github.com>
2022-03-23 22:07:37 +0900

    フォーム

--- db/migrate/20220319142104_create_clothes.rb
+++ db/migrate/20220319142104_create_clothes.rb
@@ -2,7 +2,7 @@ class CreateClothes < ActiveRecord::Migration[6.0]
   def change
     create_table :clothes do |t|
       t.string :memo
-      t.string :date
+      t.date :day
       t.string :season, null: false
       t.string :color, null: false
       t.timestamps
